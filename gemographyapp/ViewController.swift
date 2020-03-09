962fc57eced5563a3749efeeeb690b43fc8395f8
simo <simomario22@gmail.com>
2020-03-09 17:45:37 +0100

    setup tableview

--- gemographyapp/ViewController.swift
+++ gemographyapp/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        setuptableview ()
     }
 
+    fileprivate func setuptableview () {
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell_id")
+        
+    }
 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+     return 40
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_id", for: indexPath)
+        
+        cell.textLabel?.text = "simo simo"
+        return cell
+    }
+    
 }
 
